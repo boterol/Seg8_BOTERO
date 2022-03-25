be33b61a84e977954ecf9f8a7b678ad542f8afe5
Luis Fernando Botero <luis.botero2001@gmail.com>
2022-03-24 21:02:33 -0500

    hola

--- Seg8/src/ui/Main.java
+++ Seg8/src/ui/Main.java
@@ -1,20 +1,23 @@
 package ui;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import model.CountryData;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("SEGUIMIENTO 8, LUIS FERNANDO BOTERO\n\n");	
-		
+		Scanner sc=new Scanner(System.in);
 		CountryData cd=new CountryData();
-		cd.importData("C:\\Users\\luisb\\eclipse-workspace\\Seg8\\infoOlimpicos.txt");
+		String path;
+		System.out.println("SEGUIMIENTO 8, LUIS FERNANDO BOTERO\n\n");	
+		System.out.println("pegue acá la ruta del archivo txt.");
+		path=sc.nextLine();
 		
-		System.out.println(cd.toString());
-		
-		//System.out.println(cd.data);
+		cd.importData(path);
+
+		System.out.println("\n\n"+cd.toString());
 	}
 
 }
